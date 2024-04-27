32b7145c172ab3f831031385859919d074a48ab2
Gulnar Rahimli <146723639+gulrah@users.noreply.github.com>
2024-04-27 12:58:17 -0700

    Deleted blanklines

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -8,16 +8,16 @@ from sys import argv
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/users"
     users = requests.get(url).json()
-    
+
     all_tasks = {}
-    
+
     for user in users:
         user_id = user.get("id")
         username = user.get("username")
-        
+
         url = f"https://jsonplaceholder.typicode.com/todos?userId={user_id}"
         tasks = requests.get(url).json()
-        
+
         user_tasks = []
         for task in tasks:
             task_dict = {
@@ -26,7 +26,7 @@ if __name__ == "__main__":
                 "completed": task.get("completed")
             }
             user_tasks.append(task_dict)
-            
+
         all_tasks[user_id] = user_tasks
 
     with open("todo_all_employees.json", "w") as json_file:
