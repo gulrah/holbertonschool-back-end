9b1631ae51a60a2953fe75ba59479260c608a385
Gulnar Rahimli <146723639+gulrah@users.noreply.github.com>
2024-04-28 06:48:10 -0700

    Update 0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -2,36 +2,31 @@
 
 """Script"""
 
-import requests
-import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 {} <employee_id>".format(sys.argv[0]))
-        sys.exit(1)
 
-    employee_id = sys.argv[1]
-    base_url = "https://jsonplaceholder.typicode.com"
-    user_url = base_url + "/users/{}".format(employee_id)
-    todo_url = base_url + "/todos?userId={}".format(employee_id)
+    import requests
+    from sys import argv
+    if len(argv) < 2:
+        exit()
+    todos = requests.get(
+        "https://jsonplaceholder.typicode.com/todos?userId={}&completed=true"
+        .format(argv[1]))
+    name = requests.get(
+        "https://jsonplaceholder.typicode.com/users?id={}"
+        .format(argv[1]))
+    name = name.json()
+    name = name[0]["name"]
+    todo = requests.get(
+        "https://jsonplaceholder.typicode.com/todos?userId={}".format(argv[1]))
+    todo = todo.json()
+    todo = len(todo)
+    todos = todos.json()
+    todo_list = []
 
-    try:
-        user_response = requests.get(user_url)
-        user_response.raise_for_status()
-        user_data = user_response.json()
-
-        todo_response = requests.get(todo_url)
-        todo_response.raise_for_status()
-        todo_data = todo_response.json()
-
-        done_tasks = [task for task in todo_data if task['completed']]
-        total_tasks = len(todo_data)
-        num_done_tasks = len(done_tasks)
-
-        print("Employee {} is done with tasks({}/{}):"
-            .format(user_data['name'], num_done_tasks, total_tasks))
-        for task in done_tasks:
-            print("\t{}".format(task['title']))
-
-    except requests.exceptions.RequestException as e:
-        print("Error fetching data:", e)
+    for x in todos:
+        todo_list.append("\t {}".format(x["title"]))
+    print("Employee {} is done with tasks({}/{}):"
+          .format(name, len(todos), todo))
+    for y in todo_list:
+        print(y)
