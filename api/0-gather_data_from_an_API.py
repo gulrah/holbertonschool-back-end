849ce943525ab15ba30d4fd38be8af1df226d743
Gulnar Rahimli <146723639+gulrah@users.noreply.github.com>
2024-04-28 06:20:20 -0700

    Update 0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -5,32 +5,32 @@
 import requests
 import sys
 
-def fetch_todo_list(employee_id):
-    base_url = 'https://jsonplaceholder.typicode.com'
-    user_url = f'{base_url}/users/{employee_id}'
-    todos_url = f'{base_url}/todos?userId={employee_id}'
-    
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python3 {} <employee_id>".format(sys.argv[0]))
+        sys.exit(1)
+
+    employee_id = sys.argv[1]
+    base_url = "https://jsonplaceholder.typicode.com"
+    user_url = base_url + "/users/{}".format(employee_id)
+    todo_url = base_url + "/todos?userId={}".format(employee_id)
+
     try:
         user_response = requests.get(user_url)
-        todos_response = requests.get(todos_url)
+        user_response.raise_for_status()
         user_data = user_response.json()
-        todos_data = todos_response.json()
-        
-        employee_name = user_data['name']
-        total_tasks = len(todos_data)
-        completed_tasks = [task['title'] for task in todos_data if task['completed']]
-        
-        print(f"Employee {employee_name} is done with tasks({len(completed_tasks)}/{total_tasks}):")
-        for task in completed_tasks:
-            print(f"\t{task}")
+
+        todo_response = requests.get(todo_url)
+        todo_response.raise_for_status()
+        todo_data = todo_response.json()
+
+        done_tasks = [task for task in todo_data if task['completed']]
+        total_tasks = len(todo_data)
+        num_done_tasks = len(done_tasks)
+
+        print("Employee {} is done with tasks({}/{}):".format(user_data['name'], num_done_tasks, total_tasks))
+        for task in done_tasks:
+            print("\t{}".format(task['title']))
+
     except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
-        sys.exit(1)
-        
-if __name__ == '__main__':
-    if len(sys.argv) != 2 or not sys.argv[1].isdigit():
-        print("Usage: ./0-gather_data_from_an_API.py <employee_id>")
-        sys.exit(1)
-        
-    employee_id = int(sys.argv[1])
-    fetch_todo_list(employee_id)
+        print("Error fetching data:", e)
